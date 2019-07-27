193a7f54b95d64be9540e7da0921deaae0079e54
林颖 <linying@i-counting.cn>
2019-07-27 18:37:28 +0800

    [feat] 单独使用redux拆分出actions related to issue #1

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import * as serviceWorker from './serviceWorker';
 
 import { createStore } from 'redux';
 import reducer from './reducers/counter';
+import { increment, decrement } from './actions'
 
 /** 创建store */
 const store = createStore(reducer);
@@ -21,8 +22,8 @@ const render = () => {
   ReactDOM.render(
     <App
       value={ store.getState() }
-      onIncrement={() => store.dispatch( {type: 'INCREMENT'} )}
-      onDecrement={() => store.dispatch( {type: 'DECREMENT'})}
+      onIncrement={() => store.dispatch(increment())}
+      onDecrement={() => store.dispatch(decrement())}
     />, document.getElementById('root'));
 }
 
