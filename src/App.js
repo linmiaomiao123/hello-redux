18ab4a2a5b00c732d129c7a23d0553b4691f8ee6
林颖 <linying@i-counting.cn>
2019-07-29 15:00:17 +0800

    [feat] 触发action 方法三/四 connect 传第二个参数，mapDispatchToProps, 借用bindActionCreators related to issue #1

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { increment } from './actions';
+import { bindActionCreators } from 'redux';
 
 class App extends React.Component {
   render () {
-    const { a } = this.props
+    const { increment } = this.props
     return (
       <div className='container'>
         <h1 className='jumbotron-heading text-center'>{this.props.counter}</h1>
         <p className='text-center'>
-          <button onClick={() => a({id: 1, name: 'lin'})} className='btn btn-primary mr-2'>Increase</button>
+          <button onClick={() => increment({id: 1, name: 'lin'})} className='btn btn-primary mr-2'>Increase</button>
           <button className='btn btn-danger mr-2'>Decrease</button>
         </p>
       </div>
@@ -25,14 +26,28 @@ const mapStateToProps = (state) => {
   }
 }
 
+/** 方法二 */
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increment: (name) => dispatch(increment(name))
+//   }
+// }
+
+/** 方法三 */
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increment: bindActionCreators(increment, dispatch)
+//   }
+// }
+
+/** 方法四 */
 const mapDispatchToProps = (dispatch) => {
-  return {
-    a: (name) => dispatch(increment(name))
-  }
+  return bindActionCreators({ increment}, dispatch)
 }
 
 App.propTypes = {
   counter: PropTypes.number.isRequired
 }
 
+/** 方法2/3/4 */
 export default connect(mapStateToProps, mapDispatchToProps)(App);
